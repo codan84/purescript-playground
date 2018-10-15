84f0d22d249232f8951c90cae8926c19974f296e
Daniel Gruszczyk <daniel.gruszczyk@live.com>
2018-10-15 09:33:58 +0100

    Example module and function

---
+++ src/DoStuff.purs
@@ -0,0 +1,6 @@
+module DoStuff where
+  
+import Prelude
+
+doStuff :: Int -> Int -> Int
+doStuff a b = a * b
--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,8 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
+import DoStuff (doStuff)
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  log ("Hello sailor! " <> show (doStuff 3 7))
